1d6fff5608aafd818e55d73c0cf9dfbbeefdb890
Ameer Deen <writeameer@gmail.com>
2025-06-28 19:00:13 +0400

    Cleaning up

--- Program.cs
+++ Program.cs
@@ -1,26 +1,6 @@
-// This is a .NET gRPC plugin for dstream that implements the HashiCorp go-plugin handshake protocol
-// It outputs an infinite counter every 5 seconds and ensures proper handshake with dstream
-// This version uses a simplified plugin framework for easier plugin development
-using System;
-using System.Threading.Tasks;
+using DStreamDotnetTest;
 
-namespace DStreamDotnetTest
-{
-    /// <summary>
-    /// Main program class
-    /// </summary>
-    public class Program
-    {
-        /// <summary>
-        /// Entry point for the application
-        /// </summary>
-        static async Task Main(string[] args)
-        {
-            // Create the plugin host for our CounterPlugin
-            var host = new DStreamPluginHost<CounterPlugin>();
-            
-            // Run the plugin host
-            await host.RunAsync(args);
-        }
-    }
-}
+var host = new DStreamPluginHost<CounterPlugin>();
+
+// Run the plugin host
+await host.RunAsync(args);
