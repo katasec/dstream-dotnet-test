1825f32f68802ae31b29ae0dae99eca6366f4a97
Ameer Deen <writeameer@gmail.com>
2025-08-27 15:59:24 +0400

    Moved providers out:

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using DStreamDotnetTest;
-using DStreamDotnetTest.Providers;
 using Katasec.DStream.Plugin;
-
-// Register providers
-ProviderRegistry.RegisterInputProvider<NullInputProvider>("null");
-ProviderRegistry.RegisterOutputProvider<ConsoleOutputProvider>("console");
+using Katasec.DStream.Providers;
+using Katasec.DStream.Providers.Input;
+using Katasec.DStream.Providers.Output;
 
 // Create and run the plugin host with strongly-typed configuration
-var host = new DStreamPluginHost<GenericCounterPlugin, GenericCounterConfig>();
+var plugin = new GenericCounterPlugin();
+var inputProvider = Katasec.DStream.Providers.ProviderRegistry.GetInputProvider("null");
+var outputProvider = Katasec.DStream.Providers.ProviderRegistry.GetOutputProvider("console");
+
+var host = Katasec.DStream.Plugin.PluginHostFactory.CreatePluginHost<GenericCounterPlugin, GenericCounterConfig>(
+    plugin, inputProvider, outputProvider);
+    
 await host.RunAsync(args);
